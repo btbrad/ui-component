324308b9974d80d1a6bd10f6cee49359335aba56
bt <biantaobrad@gmail.com>
2020-05-04 19:14:20 +0800

    添加icon组件

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import Button from './button'
+import Icon from './icon.vue'
 
 Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
 
 let vm = new Vue({
     el: '#app'
---
+++ src/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="icon">
+      <use :xlink:href="`#i-${name}`"></use>
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'g-icon',
+  props: {
+    name: {
+      type: String
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.icon {
+    height: 1em;
+    width: 1em;
+}
+</style>
