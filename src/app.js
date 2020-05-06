847455dd745e6ca6475aab530475578815f784b4
bt <biantaobrad@gmail.com>
2020-05-06 12:52:03 +0800

    单元测试与mock

--- src/app.js
+++ src/app.js
@@ -17,3 +17,91 @@ let vm = new Vue({
       }
     }
 })
+
+import chai from 'chai'
+
+const expect = chai.expect
+
+// 单元测试
+// icon
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  })
+  button.$mount('#test')
+
+  let useElement = button.$el.querySelector('use')
+  expect(useElement.getAttribute('xlink:href')).to.eq('#i-settings')
+}
+// loading
+{
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  })
+  button.$mount()
+
+  let useElement = button.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+}
+// oder 
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+      iconPosition: 'right'
+    }
+  })
+  button.$mount(div)
+
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('2')
+  button.$el.remove()
+  button.$destroy()
+}
+{
+  let div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const button = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  button.$mount(div)
+
+  let svg = button.$el.querySelector('svg')
+  let {order} = window.getComputedStyle(svg)
+  expect(order).to.eq('1')
+  button.$el.remove()
+  button.$destroy()
+}
+// click mock
+import spies from 'chai-spies'
+
+chai.use(spies)
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(()=>{})
+  vm.$on('click', spy)
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
+}
