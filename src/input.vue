877a48dd3e055855b7932cd88cc58e27db282bc6
bt <biantaobrad@gmail.com>
2020-05-13 23:43:06 +0800

    添加Focus阴影

--- src/input.vue
+++ src/input.vue
@@ -1,22 +1,43 @@
 <template>
   <div class="wrapper">
-    <input type="text">
+    <input type="text" :value="value">
   </div>
 </template>
 
 <script>
 export default {
-  name: 'GuruInput'
+  name: 'GuruInput',
+  props: {
+    value: {
+      type: String
+    }
+  }
 }
 </script>
 
 <style lang='scss' scoped>
   $height: 32px;
   $border-color: #999;
+  $border-color-hover: #666;
+  $border-radius: 4px;
+  $font-size: 14px;
+  $box-shadow-color: rgba(0,0,0,.5);
   .wrapper {
+    font-size: $font-size;
+    display: inline-block;
     > input {
       height: $height;
       border: 1px solid $border-color;
+      border-radius: $border-radius;
+      padding: 0 8px;
+      font-size: inherit;
+      &:hover {
+        border-color: $border-color-hover;
+      }
+      &:focus {
+        box-shadow:inset  0 1px 3px $box-shadow-color;
+        outline: none;
+      }
     }
   }
 
