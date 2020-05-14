38dce2d4f1d3f4ed51420537ffa51d2516af88b2
bt <biantaobrad@gmail.com>
2020-05-14 22:56:51 +0800

    error状态

--- src/input.vue
+++ src/input.vue
@@ -1,14 +1,33 @@
 <template>
-  <div class="wrapper">
-    <input type="text" :value="value">
+  <div class="wrapper" :class="{error}">
+    <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+    <template v-if="error">
+      <g-icon name="error" class="icon-error"></g-icon>
+      <span class="error-message">{{error}}</span>
+    </template>
   </div>
 </template>
 
 <script>
+import Icon from './icon'
 export default {
   name: 'GuruInput',
+  components: {
+    GIcon: Icon
+  },
   props: {
     value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    error: {
       type: String
     }
   }
@@ -22,9 +41,19 @@ export default {
   $border-radius: 4px;
   $font-size: 14px;
   $box-shadow-color: rgba(0,0,0,.5);
+  $red: #F1453D;
   .wrapper {
     font-size: $font-size;
-    display: inline-block;
+    display: inline-flex;
+    align-items: center;
+    > :not(:last-child) {
+      margin-right: 0.5em;
+    }
+    &.error {
+      > input {
+        border-color: $red;
+      }
+    }
     > input {
       height: $height;
       border: 1px solid $border-color;
@@ -38,7 +67,24 @@ export default {
         box-shadow:inset  0 1px 3px $box-shadow-color;
         outline: none;
       }
+      &[disabled] {
+        border-color: #ccc;
+        color: #ccc;
+        cursor: not-allowed;
+      }
+      &[readonly] {
+        border-color: #ccc;
+        color: #ccc;
+        cursor: not-allowed;
+      }
     }
   }
-
+  .icon-error {
+    fill: $red;
+    vertical-align: middle;
+  }
+  .error-message {
+    color: $red;
+    vertical-align: middle;
+  }
 </style>
