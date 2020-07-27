34d96971dc70489cde103fdb7164c193b7bade08
btbrad <biantaobrad@gmail.com>
2020-07-27 22:31:49 +0800

    开始popover

---
+++ src/popover.vue
@@ -0,0 +1,27 @@
+<!--
+ * @Author: btbrad
+ * @Date: 2020-07-27 22:09:07
+ * @LastEditors: btbrad
+ * @LastEditTime: 2020-07-27 22:17:27
+ * @Description: 
+--> 
+<template>
+  <div class="popover">
+    <slot name="content"></slot>
+    <slot name="trigger"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GuruPopover'
+}
+</script>
+
+<style scoped lang="scss">
+.popover {
+  display: inline-block;
+  vertical-align: top;
+}
+</style>
+
