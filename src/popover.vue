977e9bfc07301c4e2e1b0126c181f5276eba967f
bt <biantaobrad@gmail.com>
2020-07-29 11:23:37 +0800

    popover 样式

--- src/popover.vue
+++ src/popover.vue
@@ -6,9 +6,12 @@
  * @Description: 
 --> 
 <template>
-  <div class="popover">
-    <div class="content">
-      <slot v-if="visible" name="content"></slot>
+  <div class="popover" ref="popover">
+    <div v-if="visible" ref="content" class="content">
+      <div class="content-wrapper">
+        <slot name="content"></slot>
+        <div class="arrow-down"></div>
+      </div>
     </div>
     <div class="trigger" ref="trigger">
       <slot name="trigger"></slot>
@@ -41,20 +44,54 @@ export default {
       this.bind()
     })
   },
+  computed: {
+    position() {
+      return this.$refs.trigger.getBoundingClientRect()
+    }
+  },
   methods: {
     show() {
       this.visible = true
+      // this.$nextTick(() => {
+      //   let { x: left } = this.position
+      //   console.log(this.position)
+      //   this.$refs.content.style.left = left + 'px'
+      // })
     },
     hide() {
       this.visible = false
     },
     bind() {
-      this.trigger === 'hover' && this.$refs.trigger.addEventListener('mouseenter', () => {
-        this.show()
-      })
-      this.trigger === 'hover' && this.$refs.trigger.addEventListener('mouseleave', () => {
-        this.hide()
-      })
+      if (this.trigger === 'hover') {
+        this.$refs.trigger.addEventListener('mouseenter', (e) => {
+          this.show()
+          e.stopPropagation()
+          if (this.visible) {
+            this.$nextTick(() => {
+              this.$refs.content.addEventListener('mouseenter', (e) => {
+                this.show()
+                e.stopPropagation()
+              })
+              this.$refs.content.addEventListener('mouseleave', (e) => {
+                this.hide()
+                e.stopPropagation()
+              })
+              // console.log(this.position)
+            })
+          }
+        })
+        this.$refs.trigger.addEventListener('mouseleave', () => {
+          this.hide()
+        })
+        document.addEventListener('mouseover', (event) => {
+          let dom = event.target
+          event.stopPropagation()
+          if (this.$refs.popover && this.$refs.popover.contains(dom)) {
+            return
+          }
+          this.hide()
+        })
+      }
     },
   }
 }
@@ -67,8 +104,41 @@ export default {
   position: relative;
   .content {
     position: absolute;
-    top: -100%;
-    border: 1px solid #ccc;
+    bottom: 100%;
+    min-width: 100px;
+    // border: 1px solid #f40;
+    padding-bottom: 10px;
+    .content-wrapper {
+      border: 1px solid #999;
+      padding: 10px;
+      position: relative;
+      .arrow-down {
+        display: block;
+        position: absolute;
+        border: 1px solid #999;
+        width: 0;
+        height: 0;
+        bottom: -12px;
+        left: 50%;
+        transform: translateX(-50%);
+        border-top: 10px solid #999;
+        border-left: 10px solid transparent;
+        border-right: 10px solid transparent;
+        &::after {
+          content: '';
+          display: block;
+          position: absolute;
+          width: 0;
+          height: 0;
+          top: -11px;
+          left: 50%;
+          transform: translateX(-50%);
+          border-top: 9px solid #fff;
+          border-left: 9px solid transparent;
+          border-right: 9px solid transparent;
+        }
+      }
+    }
   }
 }
 </style>
