732c8472df745413ba5d37b945ef25b51fe8b165
btbrad <biantaobrad@gmail.com>
2020-08-11 22:50:48 +0800

    使用vue-cli3构建

---
+++ src/demo.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: btbrad
+ * @Date: 2020-08-11 22:12:52
+ * @LastEditors: btbrad
+ * @LastEditTime: 2020-08-11 22:49:25
+ * @Description: 
+-->
+<template>
+  <div>
+    <h2>Guru-UI</h2>
+    <hr />
+    <div style="margin-top: 30px;margin-left: 200px;width: 500px;">
+      <g-collapse :selected.sync="selectedCollapse" type="single">
+        <g-collapse-item title="标题1" name="1">内容1</g-collapse-item>
+        <g-collapse-item title="标题2" name="2">内容2</g-collapse-item>
+        <g-collapse-item title="标题3" name="3">内容3</g-collapse-item>
+      </g-collapse>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Demo',
+}
+</script>
+
+<style></style>
---
+++ src/main.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+// import 'core-js/shim'
+// import '@babel/polyfill'
+
+import Demo from './demo.vue'
+
+import Button from './button.vue'
+import Icon from './icon.vue'
+import GButtonGroup from './button-group.vue'
+import GInput from './input.vue'
+import GRow from './row.vue'
+import GCol from './col.vue'
+import GLayout from './layout.vue'
+import GHeader from './header.vue'
+import GSider from './sider.vue'
+import GContent from './content.vue'
+import GFooter from './footer.vue'
+import GToast from './toast.vue'
+import plugin from './plugin'
+import Tabs from './tabs.vue'
+import TabsNav from './tabs-nav.vue'
+import TabsContent from './tabs-content.vue'
+import TabsPane from './tabs-pane.vue'
+import TabsItem from './tabs-item.vue'
+import Popover from './popover.vue'
+import Collapse from './collapse.vue'
+import CollapseItem from './collapseItem.vue'
+
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
+Vue.component('g-button-group', GButtonGroup)
+Vue.component('g-input', GInput)
+Vue.component('g-row', GRow)
+Vue.component('g-col', GCol)
+Vue.component('g-layout', GLayout)
+Vue.component('g-header', GHeader)
+Vue.component('g-sider', GSider)
+Vue.component('g-content', GContent)
+Vue.component('g-footer', GFooter)
+Vue.component('g-toast', GToast)
+Vue.component('g-tabs', Tabs)
+Vue.component('g-tabs-nav', TabsNav)
+Vue.component('g-tabs-content', TabsContent)
+Vue.component('g-tabs-pane', TabsPane)
+Vue.component('g-tabs-item', TabsItem)
+Vue.component('g-popover', Popover)
+Vue.component('g-collapse', Collapse)
+Vue.component('g-collapse-item', CollapseItem)
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: (h) => h(Demo),
+}).$mount('#app')
