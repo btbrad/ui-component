7450703c36a77ee41ef0c188dc6c6120e2f27d52
btbrad <biantaobrad@gmail.com>
2021-01-18 00:16:31 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import  Button from './src/button.vue'
-import  ButtonGroup from './src/button.vue'
-import  Icon from './src/icon.vue'
+import Button from './src/button.vue'
+import ButtonGroup from './src/button-group.vue'
+import Icon from './src/icon.vue'
 
 export { ButtonGroup, Button, Icon }
