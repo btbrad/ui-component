5d1c1803299e62a6460245fdeb2cecaba4a25568
btbrad <biantaobrad@gmail.com>
2021-03-15 20:02:39 +0800

    确定递归组件结构

--- src/cascader-item.vue
+++ src/cascader-item.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="cascader-item">
-    <div v-for="(item, index) in source" :key="index">
-      <p>{{ item && item.name }}</p>
-      <guru-cascader-item v-if="item.children" :source="item.children" />
+    <div class="left">
+      <div v-for="(item, index) in source" :key="index" @click="leftSelected = item">
+        <p>{{ item && item.name }}</p>
+      </div>  
+    </div>
+    <div class="right" v-if="rightItems">
+      <guru-cascader-item :source="rightItems" />
     </div>
   </div>
 </template>
@@ -15,12 +19,28 @@ export default {
       type: Array,
       default: () => []
     }
+  },
+  data() {
+    return {
+      leftSelected: null
+    }
+  },
+  computed: {
+    rightItems () {
+      if (this.leftSelected && this.leftSelected.children) {
+        return this.leftSelected.children
+      } else {
+        return null
+      }
+    }
   }
 }
 </script>
 
 <style lang="scss" scoped>
 .cascader-item {
-  width: 120px;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
 }
 </style>
